4835a9fd3ec91599ebaf738ac58009c4926ec89f
Victor Adeyemo <billionaireteacher@gmail.com>
2024-01-02 14:12:38 -0800

    Upload first task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#inlcude "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int and updates the value to 98
+ * @n: integer parameter
+ *
+ * Return: return nothing
+ */
+void reset_to_98(int *n)
+{
+	n = 98;
+}
