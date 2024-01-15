1310be4553c996f8c39c33084f19ca96911dd388
Victor Adeyemo <billionaireteacher@gmail.com>
2024-01-15 04:59:23 -0800

    Solve task 0 and 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ *
+ * @argc: argument count
+ * @argv: argument value
+ * Return: Always (0);
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
